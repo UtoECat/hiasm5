74280315ef5eef27670e0956a0a2e4d4a075163e
UtoECat <utopia.egor.cat.allandall@gmail.com>
2024-10-23 14:21:07 +0300

    +1 fix

--- build_msys.sh
+++ build_msys.sh
@@ -3,4 +3,10 @@
 
 export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig
 PLATFORM_PREFIX="mingw-w64-x86_64-"
-pacman -S "${PLATFORM_PREFIX}gtksourceviewmm3" "${PLATFORM_PREFIX}gdk-pixbuf2" "${PLATFORM_PREFIX}sqlite3" "${PLATFORM_PREFIX}gtkmm"
+pacman -S "${PLATFORM_PREFIX}gtksourceviewmm3" "${PLATFORM_PREFIX}gdk-pixbuf2" "${PLATFORM_PREFIX}sqlite3" "${PLATFORM_PREFIX}gtkmm" "${PLATFORM_PREFIX}cmake" "${PLATFORM_PREFIX}make"
+
+mkdir build
+cd build
+
+cmake.exe ..
+make.exe
