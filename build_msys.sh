6dd1a7c224829aae7a4f1175d1fa1cb33023499b
UtoECat <utopia.egor.cat.allandall@gmail.com>
2024-10-23 14:20:15 +0300

    test1

---
+++ build_msys.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# THIS SCRIPT uses msys2 enviroment with mingw to build this app
+
+export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig
+PLATFORM_PREFIX="mingw-w64-x86_64-"
+pacman -S "${PLATFORM_PREFIX}gtksourceviewmm3" "${PLATFORM_PREFIX}gdk-pixbuf2" "${PLATFORM_PREFIX}sqlite3" "${PLATFORM_PREFIX}gtkmm"
